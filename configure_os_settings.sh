43674104458c703201dae2681ead72b1350bb346
Horia Constantin <horia.constantin@icloud.com>
2019-01-24 21:41:44 +0700

    added setting to remove remote image loading in apple mail

--- configure_os_settings.sh
+++ configure_os_settings.sh
@@ -3,14 +3,18 @@
 # This script changes some of the default macOS settings
 # Reference: https://github.com/kevinSuttle/macOS-Defaults/blob/master/.macos
 
-### UI ###
+###############################################################################
+### UI
+###############################################################################
 echo "Setting always show scrollbars" # Possible values: `WhenScrolling`, `Automatic` and `Always`
 defaults write NSGlobalDomain AppleShowScrollBars -string "Always"
 echo "Setting expand save panel by default"
 defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
 defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
 
-### Keyboard ###
+###############################################################################
+### Keyboard
+###############################################################################
 echo "Disable smart quotes"
 defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
 echo "Disable smart dashes"
@@ -20,12 +24,20 @@ defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
 echo "Disable automatic capitalization"
 defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
 
-### Finder ###
+###############################################################################
+### Finder
+###############################################################################
 echo "Setting Finder: show hidden files by default"
 defaults write com.apple.finder AppleShowAllFiles -bool true
 echo "Setting Finder: show path bar"
 defaults write com.apple.finder ShowPathbar -bool true
 
+###############################################################################
+### Mail
+###############################################################################
+
+echo "Manual step: Disabling Remote Content in Apple Mail"
+echo "Launch Mail in OS X and go to Mail > Preferences > Viewing. Find the box labeled Load remote content in messages and uncheck it."
 
 printf "\n### MANUAL operations ###\n"
 echo "To enable trackpad tap to click, do it via interface because the cli seems to be macOS version dependent"
