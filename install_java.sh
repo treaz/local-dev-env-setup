9155bec7f400d9a77f88a1684253f942b93e37a0
Horia Constantin <horia.constantin@icloud.com>
2019-02-08 15:36:26 +0700

    registering java sdk with jenv

--- install_java.sh
+++ install_java.sh
@@ -13,6 +13,11 @@ brew install jenv
 echo 'export PATH="$HOME/.jenv/bin:$PATH"' >> ~/.bash_profile
 echo 'eval "$(jenv init -)"' >> ~/.bash_profile
 
+echo "Registering java8 with jenv"
+jenv add /Library/Java/JavaVirtualMachines/jdk1.8.0_192.jdk/Contents/Home/
+echo "Registering java11 with jenv"
+jenv add /Library/Java/JavaVirtualMachines/openjdk-11.0.1.jdk/Contents/Home/
+
 # Install maven
 brew install maven
 echo "RUN MANUALLY: jenv enable-plugin maven"
