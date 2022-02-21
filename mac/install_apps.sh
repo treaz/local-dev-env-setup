4eabe8186de03ed257a72970e6de860a55778d88
Horia Constantin <me@horiaconstantin.com>
2022-02-21 09:22:12 +0100

    Update install_apps.sh

--- mac/install_apps.sh
+++ mac/install_apps.sh
@@ -4,48 +4,33 @@ source ./setup_homebrew.sh
 # This script installs the apps that I use
 
 ### Generic apps ###
-brew cask install evernote
+brew cask install joplin
 brew cask install firefox
-brew cask install google-chrome
+brew cask install brave-browser
 brew cask install slack
 brew cask install vlc
-brew cask install skype
-brew cask install teamviewer
+brew cask install zoom
 # Window manager (with keyboard shortcuts)
 brew cask install spectacle
-brew cask install signal
 # Full featured file archiver
 brew cask install keka
 # Window position manager (for multi-monitor setup)
-brew cask install stay
+#brew cask install stay
 # File storage size viewer
-brew cask install grandperspective
+#brew cask install grandperspective
 # Anti-RSI & fatigue break manager
 brew cask install time-out
 # Professional image editing tool
-brew cask install gimp
-
-###############################################################################
-### Paid apps
-###############################################################################
-# PAID network traffic manager (limits application access)
-brew cask install tripmode
-# PAID VPN client
-brew cask install viscosity
-# PAID cloud storage service
-brew cask install stack
+#brew cask install gimp
 
 ### Development apps ###
 brew cask install sourcetree
-brew cask install visual-studio-code
+brew cask install vscodium
 brew cask install iterm2
-brew cask install graphql-playground
 brew cask install postman
-brew cask install virtualbox
-brew cask install kitematic
 # GUI for lsof - displays all open files and sockets in use by all running processes on your system
 brew cask install sloth
 
 # Wireshark app and cli
-brew install wireshark
-brew cask install wireshark
+#brew install wireshark
+#brew cask install wireshark
