43bdf479ac0620a1daf19ebe34e2e166a5b29ec7
Horia Constantin <me@horiaconstantin.com>
2022-02-21 09:29:53 +0100

    Update setup_homebrew.sh

--- mac/setup_homebrew.sh
+++ mac/setup_homebrew.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-# Change shell to bash (as default for macos is zsh
-chsh -s /bin/bash
-
 # Ask for the administrator password upfront.
 sudo -v
+
+# Change shell to bash (as default for macos is zsh
+chsh -s /bin/bash
+# Silence the ZSH warning
+echo 'export BASH_SILENCE_DEPRECATION_WARNING=1' > ~/.bash_profile
 
 # Keep-alive: update existing `sudo` time stamp until the script has finished.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
